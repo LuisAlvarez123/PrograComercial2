0f12024f175e454aaf2833eccdba9286447e7b93
Luis Alvarez <luisjoa@mesoamericana.edu.gt>
2021-10-19 12:37:41 -0700

    finalizacion de progreyecto de django girl

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.publicacion_lista, name='publicacion_lista'),
     path('publicacion/<int:pk>/', views.publicacion_detalle, name='publicacion_detalle'),
+    path('publicacion/nueva', views.publicacion_nueva, name='publicacion_nueva'),
+    path('publicacion/<int:pk>/editar/', views.publicacion_editar, name='publicacion_editar'),
+    path('borradores', views.publicacion_borrador_lista, name= 'publicacion_borrador_lista'),
+    path('publicacion/<int:pk>/publicar/', views.publicacion_publicar, name='publicacion_publicar'),
+    path('publicacion/<int:pk>/borrar/', views.publicacion_borrar, name='publicacion_borrar'),
+
 ]
--- misitio/urls.py
+++ misitio/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.auth import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(template_name='blog/registration/login.html'), name='login'),
     path('', include('blog.urls')),
 ]
